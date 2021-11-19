4b6d2c97044b9cfe310c3fdd39873fa849c7c3b7
Florian Krull <flo.krull@gmail.com>
2021-11-19 17:14:17 +0100

    cmdl args + status checking

--- tsdimport.sh
+++ tsdimport.sh
@@ -10,24 +10,76 @@ set -ETeuo pipefail
 # TODO:
 #  support dirs?
 #  multiple files/dirs as agruments
-#  -u -g -p (user/group/project)
-#  derive project from user
-#  derive group from project
+#  reauth after token expires
+
+
+jwtfile=/tmp/USERNAME_jwt
+
+usage() {
+  printf "usage: $(basename $0) -u user [-p project] [-g group] file\n"
+  printf "  remove '${jwtfile}' to reauthenticate\n"
+}
+
+argument_error() {
+  printf "error: missing arguments\n\n" >&2
+  usage
+  exit 1;
+}
+
+inp_group=
+inp_username=
+inp_project=
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -u|--username)
+      [ ! -z ${2+x} ] || argument_error
+      inp_username=$2
+      shift
+      shift
+      ;;
+    -g|--group)
+      [ ! -z ${2+x} ] || argument_error
+      inp_group=$2
+      shift
+      shift
+      ;;
+    -p|--project)
+      [ ! -z ${2+x} ] || argument_error
+      inp_project=$2
+      shift
+      shift
+      ;;
+    *)
+      break
+  esac
+done
+
+[ ! -z ${inp_username} ] || { printf "error: missing user name\n\n" >&2; usage; exit 1; }
 
 # config
-project="p22"
-username="${project}-floriakr"
-group="${project}-member-group"
+username=${inp_username}
+project=${inp_project}
+: ${project:=${username%%-*}} # derive project by taking pXXX-username prefix
+group=${inp_group}
+: ${group:="${project}-member-group"} # derive project by taking pXXX-username prefix
+
+
+# echo "username:" $username
+# echo "project:" $project
+# echo "group:" $group
+
 jwtfile=/tmp/${username}_jwt
+
+[ ! -z ${1+x} ] || { printf "error: missing arguments\n\n" >&2; usage; exit 1; }
 
 inputfilepath=${1}
 inputfilename=${inputfilepath##*/}
 
-if [ ! -f "${inputfilepath}" ]; then
-  printf "usage: $(basename $0) <file>\n\n"
-  printf "remove '${jwtfile}' to reauthenticate\n"
-  exit 1
-fi
+[ -f "${inputfilepath}" ] || { printf "error: file \"${inputfilepath}\" does not exists.\n" >&2; exit 1; }
+
 
 # request access token?
 if [ ! -f ${jwtfile} ]; then
@@ -36,33 +88,41 @@ if [ ! -f ${jwtfile} ]; then
   echo
   read -p "OTP:" otp
 
-  read -d '' reqdata << EOF
+  reqdata=$(cat << EOF
 {
   "user_name":"${username}",
   "password":"${password}",
   "otp":"${otp}"
 }
 EOF
+)
   echo "$reqdata"
   jwt="$(
     curl "https://data.tsd.usit.no/${project}/import" \
     -X POST \
     -H 'Content-Type: application/json' \
-    --data-raw "${reqdata}"
+    --data-raw "${reqdata}" \
+    -v
   )"
+  echo $jwt
+  # check response
+  { echo "${jwt}" | jq -e 'has("token")'; } || { printf "error: couldn't get access token\n\n" >&2; exit 1; }
+  # write token file
   echo "${jwt}" | jq --raw-output .token > ${jwtfile}
 fi
 
-# print access token
-echo "$jwt"
-
 # upload file 
 jwt="$( cat ${jwtfile} )"
-curl "https://data.tsd.usit.no/v1/${project}/files/stream/${inputfilename}?group=${group}" \
-  -X PUT \
-  -H "Authorization: Bearer $jwt" \
-  -H 'Cookie: import=' \
-  --data-binary "@${inputfilepath}" \
-  -v
+r="$(
+  curl "https://data.tsd.usit.no/v1/${project}/files/stream/${inputfilename}?group=${group}" \
+    -X PUT \
+    -H "Authorization: Bearer $jwt" \
+    -H 'Cookie: import=' \
+    --data-binary "@${inputfilepath}" \
+    -v
+  )"
+echo "$r"
+# check response
+{ echo "${r}" | jq -e '.message | test("data streamed")'; } || { printf "error: couldn't upload file\n\n" >&2; exit 1; }
 
 echo
